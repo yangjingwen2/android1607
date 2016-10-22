845fff5ef1399239a42cd2822b3c82b30769c40f
yangjw <yangwen8701@126.com>
2016-10-22 15:16:49 +0800

    为了完成测试，在MainActivity中添加了一个TextView的初始化过程

--- app/src/main/java/com/androidxx/yangjw/gitdemo/MainActivity.java
+++ app/src/main/java/com/androidxx/yangjw/gitdemo/MainActivity.java
@@ -2,12 +2,21 @@ package com.androidxx.yangjw.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+/**
+ * android1607的Git的基本使用
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private TextView viewById;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewById = (TextView) findViewById(R.id.main_show_txt);
     }
 }
